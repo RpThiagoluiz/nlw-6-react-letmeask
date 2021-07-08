81092fb194a18c6e802fbfd522a372027c60e05a
Thiago Luiz Gonçalves <rp.thiagoluiz@gmail.com>
2021-07-08 15:09:17 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgTLLjn4kjqLl76dKyGDl8JIekjCF-nbo",
-  authDomain: "letmeask-33f4a.firebaseapp.com",
-  databaseURL: "https://letmeask-33f4a-default-rtdb.firebaseio.com",
-  projectId: "letmeask-33f4a",
-  storageBucket: "letmeask-33f4a.appspot.com",
-  messagingSenderId: "436751948623",
-  appId: "1:436751948623:web:508867e18efc8e711a033a",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
 };
 
 firebase.initializeApp(firebaseConfig);
